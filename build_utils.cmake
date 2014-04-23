87cc97ecf097e97a71fa359000e0007f11a44c64
Jacob McIntosh <nacitar@ubercpp.com>
2014-04-23 13:12:02 -0500

    More warnings silenced

--- build_utils.cmake
+++ build_utils.cmake
@@ -33,6 +33,7 @@
 # New linker directory behavior
 if(COMMAND cmake_policy)
   cmake_policy(SET CMP0003 NEW)
+  cmake_policy(SET CMP0011 NEW)
 endif(COMMAND cmake_policy)
 
 # Appends values to a space-delimited string
