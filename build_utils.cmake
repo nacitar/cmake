ec3e4e8007c82ed427447ce4d03e69b16f79778d
Jacob McIntosh <nacitar@ubercpp.com>
2014-04-23 12:47:53 -0500

    Added other target types.

--- build_utils.cmake
+++ build_utils.cmake
@@ -45,12 +45,24 @@ macro(ClearBuildValues)
   set(C_FLAGS)
   set(LINK_FLAGS)
 endmacro()
-# Adds a program, using separate C/CXX flags plus common CC flags
-function(AddProgramPrv TARGET
+# Adds a target, using separate C/CXX flags plus common CC flags
+function(AddTargetPrv TARGET TYPE
     CXX_SOURCES C_SOURCES
     CC_FLAGS CXX_FLAGS C_FLAGS
     LINK_FLAGS)
-  add_executable("${TARGET}" ${CXX_SOURCES} ${C_SOURCES})
+  set(SOURCES "${CXX_SOURCES};${C_SOURCES}")
+  if ("${TYPE}" STREQUAL "EXE")
+    add_executable("${TARGET}" ${SOURCES})
+  elseif ("${TYPE"}" STREQUAL "LIBRARY")
+    # generic library
+    add_library("${TARGET}" ${SOURCES})
+  elseif ("${TYPE}" STREQUAL "SHARED"
+      OR "${TYPE}" STREQUAL "STATIC"
+      OR "${TYPE}" STREQUAL "MODULE")
+    add_library("${TARGET}" "${TYPE}" ${SOURCES})
+  else()
+    message(FATAL_ERROR "Invalid target type: ${TYPE}")
+  endif
   set(FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}}")
   Append(FLAGS "${CC_FLAGS} ${CC_FLAGS_${CMAKE_BUILD_TYPE}}")
   Append(FLAGS "${CXX_FLAGS} ${CXX_FLAGS_${CMAKE_BUILD_TYPE}}")
@@ -60,17 +72,33 @@ function(AddProgramPrv TARGET
   Append(FLAGS "${CC_FLAGS} ${CC_FLAGS_${CMAKE_BUILD_TYPE}}")
   Append(FLAGS "${C_FLAGS} ${C_FLAGS_${CMAKE_BUILD_TYPE}}")
   set_source_files_properties(${C_SOURCES} PROPERTIES COMPILE_FLAGS "${FLAGS}")
-  set(FLAGS "${CMAKE_EXE_LINKER_FLAGS}")
-  Append(FLAGS "${CMAKE_EXE_LINKER_FLAGS_${CMAKE_BUILD_TYPE}}")
+  set(FLAGS "${CMAKE_${TYPE}_LINKER_FLAGS}")
+  Append(FLAGS "${CMAKE_${TYPE}_LINKER_FLAGS_${CMAKE_BUILD_TYPE}}")
   Append(FLAGS "${LINK_FLAGS} ${LINK_FLAGS_${CMAKE_BUILD_TYPE}}")
   set_target_properties("${TARGET}" PROPERTIES LINK_FLAGS "${FLAGS}")
 endfunction()
-# Builds a program using the globals that govern its creation
-macro(AddProgram TARGET)
-  AddProgramPrv("${TARGET}"
+# Builds a target using the globals that govern its creation
+macro(AddTarget TARGET TYPE)
+  AddTargetPrv("${TARGET}" "${TYPE}"
       "${CXX_SOURCES}" "${C_SOURCES}"
       "${CC_FLAGS}" "${CXX_FLAGS}" "${C_FLAGS}"
       "${LINK_FLAGS}")
+endmacro()
+# Macros for easily adding targets of certain types.
+macro(AddProgram TARGET)
+  AddTarget("${TARGET}" EXE)
+endmacro()
+macro(AddLibrary TARGET)
+  AddTarget("${TARGET}" LIBRARY)
+endmacro()
+macro(AddStaticLibrary TARGET)
+  AddTarget("${TARGET}" STATIC)
+endmacro()
+macro(AddSharedLibrary TARGET)
+  AddTarget("${TARGET}" SHARED)
+endmacro()
+macro(AddModule TARGET)
+  AddTarget("${TARGET}" MODULE)
 endmacro()
 # Adds additional files to the list for make clean
 function(AddCleanFiles)
